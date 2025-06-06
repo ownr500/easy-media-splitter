cf09596ad20b99735e25ef27058461588df7eb3a
ownr500 <ismediaspb@gmail.com>
2025-06-06 19:29:15 +0300

    Initial commit

---
+++ src/main/java/app/moviefx/Main.java
@@ -0,0 +1,8 @@
+package app.moviefx;
+import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        MediaPlayerFactory factory = new MediaPlayerFactory();
+    }
+}
