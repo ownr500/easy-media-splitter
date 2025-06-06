525920ad927a337ecae9ac937b2eb03173c22103
ownr500 <ismediaspb@gmail.com>
2025-06-06 20:59:00 +0300

    Add media player component

--- src/main/java/app/moviefx/Main.java
+++ src/main/java/app/moviefx/Main.java
@@ -1,11 +1,14 @@
 package app.moviefx;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class Main {
 //    private final Main thisApp;
     private final JFrame frame;
+    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 
 
     public static void main(String[] args) {
@@ -16,6 +19,8 @@ public class Main {
         frame = new JFrame("Movie Player");
         frame.setBounds(100, 100, 600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+        frame.setContentPane(mediaPlayerComponent);
         frame.setVisible(true);
     }
 }
