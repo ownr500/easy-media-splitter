ae9e54235681b8c39f60d6eae11f43b85ea816fc
ownr500 <ismediaspb@gmail.com>
2025-06-06 21:23:46 +0300

    Play sample file

--- src/main/java/app/moviefx/Main.java
+++ src/main/java/app/moviefx/Main.java
@@ -1,26 +1,24 @@
 package app.moviefx;
-import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
 public class Main {
-//    private final Main thisApp;
     private final JFrame frame;
     private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 
 
     public static void main(String[] args) {
-        Main thisApp = new Main();
+        Main thisApp = new Main(args);
     }
 
-    public Main() {
+    public Main(String[] args) {
         frame = new JFrame("Movie Player");
         frame.setBounds(100, 100, 600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
         frame.setContentPane(mediaPlayerComponent);
         frame.setVisible(true);
+        mediaPlayerComponent.mediaPlayer().media().play(args[0]);
     }
 }
