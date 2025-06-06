927bc4475dda7a84db599a63b0649d4d9553add3
ownr500 <ismediaspb@gmail.com>
2025-06-06 19:48:04 +0300

    First code

--- src/main/java/app/moviefx/Main.java
+++ src/main/java/app/moviefx/Main.java
@@ -1,8 +1,21 @@
 package app.moviefx;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Main {
+//    private final Main thisApp;
+    private final JFrame frame;
+
+
     public static void main(String[] args) {
-        MediaPlayerFactory factory = new MediaPlayerFactory();
+        Main thisApp = new Main();
+    }
+
+    public Main() {
+        frame = new JFrame("Movie Player");
+        frame.setBounds(100, 100, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
